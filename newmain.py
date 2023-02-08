c4ae2f7c1d067217b98636d4d806122716831c82
mojtaba nafez <45814367+mojtaba-nafez@users.noreply.github.com>
2023-02-09 00:46:53 +0330

    Update newmain.py

--- newmain.py
+++ newmain.py
@@ -440,6 +440,10 @@ if __name__ == '__main__':
     args = arguments_yaml.get_args()
     with open(args.work_path) as f:
         config = yaml.load(f)
+    args.joint = True
+    args.out_path = './ActiveLearning'
+    args.data_path = './cifar10/'
+    
     # convert to dict
     args = EasyDict(config)
     main(args)
